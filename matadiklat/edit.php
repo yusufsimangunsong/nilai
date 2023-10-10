53e9e63a91fe70f1b7d2b4eeaa74b459a9d13a46
yusufsimangunsong <147030924+yusufsimangunsong@users.noreply.github.com>
2023-10-10 21:38:51 +0700

    Update edit.php

--- matadiklat/edit.php
+++ matadiklat/edit.php
@@ -1,44 +1,28 @@
 <section class="content">
     <div class="row">
         <div class="col-8">
-            <h1>Modul Edit Siswa</h1>
+            <h1>Modul Edit matadiklat</h1>
         </div>
         <div class="col-4">
-            <a href="?m=siswa&s=tampil" class="btn btn-large btn-info float-end">Kembali</a>
+            <a href="?m=matadiklat&s=tampil" class="btn btn-large btn-info float-end">Kembali</a>
         </div>
         <?php
         include_once('koneksi.php');
-        $id=$_GET['id'];
-        $sql= "SELECT * FROM siswa WHERE id='$id'";
-        $query=mysqli_query($koneksi, $sql);
-        $row=mysqli_fetch_array($query);
-        //var_dump()
+        $id    = $_GET['id'];
+        $sql   = "SELECT * FROM matadiklat WHERE id='$id'";
+        $query = mysqli_query($koneksi, $sql);
+        $row = mysqli_fetch_array($query);
         ?>
         <div class="col">
-            <form action="?m=siswa&s=update" method="post">
+            <form action="?m=matadiklat&s=update" method="post">
                 <div class="mb-2">
-                    <label for="">NIS</label>
-                    <input type="number" name="nis" class"form-control" value="<?php echo $row['nis'] ?>" >
+                    <label for="">Nama matadiklat</label>
+                    <input type="text" name="matadiklat" value="<?= $row['matadiklat'] ?>" class="form-control" autofocus>
+                    <!-- value="<?// echo $row['matadiklat'] ?>"      sama wae ke di atas-->
                 </div>
                 <div class="mb-2">
-                    <label for="">Nama Siswa</label>
-                    <input type="text" name="nama" value="<?php echo $row['nama'] ?>" class="form-control" autofocus>
-                </div>
-                <div class="mb-2">
-                    <label for="">Jenis Kelamin</label>
-                    <input type="text" name="jk" class="form-control" value="<?php echo $row['jk'] ?>" >
-                </div>
-                <div class="mb-2">
-                    <label for="">Tempat Lahir</label>
-                    <input type="text" name="tempat_lahir" class="form-control" value="<?php echo $row['tempat_lahir'] ?>" >
-                </div>
-                <div class="mb-2">
-                    <label for="">Tanggal Lahir</label>
-                    <input type="text" name="tanggal_lahir" class="form-control" value="<?php echo $row['tanggal_lahir'] ?>" >
-                </div>
-                <div class="mb-2">
-                    <label for="">Jurusan</label>
-                    <input type="number" name="nama_jurusan" class="form-control" value="<?php echo $row['nama_jurusan'] ?>" >
+                    <label for="">sks</label>
+                    <input type="number" name="sks" value="<?= $row['sks'] ?>" class="form-control">
                 </div>
                 <div class="mb-2">
                     <input type="hidden" name="id" value="<?= $row['id'] ?>">
